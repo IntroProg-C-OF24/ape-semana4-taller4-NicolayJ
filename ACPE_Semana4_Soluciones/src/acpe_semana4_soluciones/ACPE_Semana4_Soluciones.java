2b699aa2c2621bea5d57b1828f75411604991b7e
NicolayJ <dnjimenez6@utpl.edu.ec>
2023-11-01 16:26:51 -0500

    ejercicio1

---
+++ ACPE_Semana4_Soluciones/src/acpe_semana4_soluciones/ACPE_Semana4_Soluciones.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package acpe_semana4_soluciones;
+
+/**
+ *
+ * @author dj931
+ */
+public class ACPE_Semana4_Soluciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
